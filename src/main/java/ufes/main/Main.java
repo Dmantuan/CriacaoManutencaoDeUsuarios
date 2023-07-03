e35783dcedb5283e594e36f2f5bd5b5a2ec4c531
Dmantuan <daniel2mantuan@gmail.com>
2023-07-03 20:30:22 -0300

    inicial

---
+++ src/main/java/ufes/main/Main.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ufes.main;
+
+/**
+ *
+ * @author daniel
+ */
+public class Main {
+    
+}
