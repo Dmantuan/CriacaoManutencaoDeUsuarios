76865f04adc25f6bc85d68042868845fcf108cc9
Dmantuan <daniel2mantuan@gmail.com>
2023-07-03 20:46:33 -0300

    inicial acertando pom

--- src/main/java/ufes/main/Main.java
+++ src/main/java/ufes/main/Main.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package ufes.main;
 
-/**
- *
- * @author daniel
- */
 public class Main {
-    
+   public static void main(String [] args){
+       System.out.println("oi");
+   }
 }
